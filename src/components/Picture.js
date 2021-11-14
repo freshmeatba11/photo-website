cc18e2f8e6a0be0971af73ec3d52ebcf4402f60b
Freshmeatba11 <cmjj8898212032@gmail.com>
2021-11-15 04:00:17 +0800

    fetch data

---
+++ src/components/Picture.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Picture = ({ data }) => {
+  return (
+    <div className="picture">
+      <p>{data.photographer}</p>
+      <div className="imageContainer">
+        <img src={data.src.large} alt="" />
+      </div>
+      <p>
+        Download Image:
+        <a target="_blank" href={data.src.large}>
+          Click Here
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Picture;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState(null);
   const auth = "563492ad6f917000010000010aba6743f6f042f791b321fafa9717f8";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const search = async () => {
+    const dataFetch = await fetch(initialURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setData(parsedData.photos);
+  };
+  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search />
+      <Search search={search} input={input} setInput={setInput} />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
     </div>
   );
 };
