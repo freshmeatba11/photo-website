fb0cce9b8490822664c279a0e02aba8f02fa27b1
Freshmeatba11 <cmjj8898212032@gmail.com>
2021-11-15 02:28:58 +0800

    css styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,7 @@ import Search from "../components/Search";
 const Homepage = () => {
   const auth = "563492ad6f917000010000010aba6743f6f042f791b321fafa9717f8";
   return (
-    <div>
+    <div style={{ minHeight: "100vh" }}>
       <Search />
     </div>
   );
