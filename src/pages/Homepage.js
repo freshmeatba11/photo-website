5c598c3ecb6965bf99ecb6bc7b02156539e92550
Freshmeatba11 <cmjj8898212032@gmail.com>
2021-11-15 23:55:43 +0800

    add more picture button

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,12 +5,14 @@ import Picture from "../components/Picture";
 const Homepage = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
   const auth = "563492ad6f917000010000010aba6743f6f042f791b321fafa9717f8";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
   //fetch data from pexels api
   const search = async (url) => {
+    setPage(2);
     const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
@@ -20,6 +22,28 @@ const Homepage = () => {
     });
     let parsedData = await dataFetch.json();
     setData(parsedData.photos);
+  };
+
+  //load more picture
+  const morepicture = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    console.log(`1. page:${page} ${newURL}`);
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    console.log(`2. page:${page} ${newURL}`);
+    let parsedData = await dataFetch.json();
+    setData([...data, ...parsedData.photos]);
   };
 
   //fetch data when the page loads up
@@ -41,6 +65,10 @@ const Homepage = () => {
           data.map((d) => {
             return <Picture data={d} />;
           })}
+      </div>
+
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More</button>
       </div>
     </div>
   );
